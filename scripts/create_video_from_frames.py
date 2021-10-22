9a0d60e3ebbfd037ca39c93b7c079359ee29373a
joan <joan.domenech.masferrer@gmail.com>
2021-10-22 15:30:57 +0200

    Create video editor class

--- scripts/create_video_from_frames.py
+++ scripts/create_video_from_frames.py
@@ -1,37 +1,7 @@
 from pathlib import Path
 
-import cv2
-import tqdm
-
 from config import _results_dir_pathlib
-
-
-def create_video_from_frames_in_dir(
-    path_to_directory, target_directory, video_name, fps
-):
-    target_directory.mkdir(parents=True, exist_ok=True)
-
-    list_of_files = sorted(path_to_directory.glob("*.png"))
-
-    img_array = []
-
-    print("\nFetching images...")
-    for file_path in tqdm.tqdm(list_of_files):
-        img = cv2.imread(str(file_path))
-        height, width, layers = img.shape
-        size = (width, height)
-        img_array.append(img)
-
-    target_video_path = target_directory / f"{video_name}.mp4"
-    out = cv2.VideoWriter(
-        str(target_video_path), cv2.VideoWriter_fourcc(*"DIVX"), fps, size
-    )
-
-    print("\nConstructing video...")
-    for i in tqdm.tqdm(range(len(img_array))):
-        out.write(img_array[i])
-    out.release()
-
+from video_editor_class import VideoEditor
 
 if __name__ == "__main__":
     path_to_dir = Path("path_to_directory_with_sequence_of_frames")
@@ -39,4 +9,8 @@ if __name__ == "__main__":
     video_name = "user_defined_video_name_without_extension"
     frame_rate = 0.5
 
-    create_video_from_frames_in_dir(path_to_dir, target_dir, video_name, frame_rate)
+    video_editor = VideoEditor(target_directory=target_dir)
+
+    video_editor.create_video_from_frames_in_dir(
+        path_to_directory=path_to_dir, target_video_name=video_name, fps=frame_rate
+    )
---
+++ video_editor_class.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import cv2
+import tqdm
+
+
+class VideoEditor(object):
+    def __init__(self, target_directory):
+        self.set_target_directory(target_directory)
+        self.current_video = None
+        self.current_video_path = None
+
+    def set_target_directory(self, target_directory):
+        self.target_directory = Path(str(target_directory))
+        target_directory.mkdir(parents=True, exist_ok=True)
+
+    def create_video_from_frames_in_dir(
+        self, path_to_directory, target_video_name, fps
+    ):
+
+        list_of_files = sorted(path_to_directory.glob("*.png"))
+
+        img_array = []
+
+        print("\nFetching images...")
+        for file_path in tqdm.tqdm(list_of_files):
+            img = cv2.imread(str(file_path))
+            height, width, layers = img.shape
+            size = (width, height)
+            img_array.append(img)
+
+        target_video_path = self.target_directory / f"{target_video_name}.mp4"
+        out = cv2.VideoWriter(
+            str(target_video_path), cv2.VideoWriter_fourcc(*"DIVX"), fps, size
+        )
+
+        print("\nConstructing video...")
+        for i in tqdm.tqdm(range(len(img_array))):
+            out.write(img_array[i])
+        out.release()
