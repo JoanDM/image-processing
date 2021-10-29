f1e410bcabfc109f6044859c42814b0ff3ee0d49
joan <joan.domenech.masferrer@gmail.com>
2021-10-29 09:11:02 +0200

    Script to batch rename frames

---
+++ scripts/batch_rename_frames.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import tqdm
+
+from config import _results_dir_pathlib
+from image_editor_class import ImageEditor
+
+
+def batch_rename_frames(path_to_directory, target_directory):
+    editor = ImageEditor(target_directory)
+    list_of_files = sorted(path_to_directory.glob("*.png"))
+
+    for i, file_path in tqdm.tqdm(enumerate(list_of_files), total=len(list_of_files)):
+        editor.set_current_img(path_to_image=file_path)
+        editor.save_current_img(target_file_name=f"{str(i).zfill(8)}")
+
+
+if __name__ == "__main__":
+    path_to_dir_with_frames = Path("path_to_dir_with_frames")
+
+    target_dir = _results_dir_pathlib / "user_defined_directory_to_store_frames"
+
+    batch_rename_frames(path_to_dir_with_frames, target_dir)
