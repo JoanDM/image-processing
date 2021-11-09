63b41ffc9ff74b8614f285177f895035c9b83002
joan <joan.domenech.masferrer@gmail.com>
2021-11-09 10:18:34 +0100

    Add argparse to create_video_from_frame_sequence script

--- scripts/create_video_from_frame_sequence_in_dir.py
+++ scripts/create_video_from_frame_sequence_in_dir.py
@@ -1,13 +1,31 @@
+import argparse
 from pathlib import Path
 
 from config import _results_dir_pathlib
 from video_editor_class import VideoEditor
 
 if __name__ == "__main__":
-    path_to_dir = Path("path_to_directory_with_sequence_of_frames")
-    target_dir = _results_dir_pathlib / "user_defined_directory_to_store_video"
-    video_name = "user_defined_video_name_without_extension"
-    frame_rate = 2
+    parser = argparse.ArgumentParser(
+        "Convert a sequence of frames in a directory to a video"
+    )
+    parser.add_argument(
+        "dir", nargs="?", help="directory containing sequence of frames"
+    )
+    args = parser.parse_args()
+
+    directory = args.dir
+
+    if directory is not None:
+        path_to_dir = Path(directory)
+        target_dir = _results_dir_pathlib / "user_defined_directory_to_store_video"
+        video_name = path_to_dir.stem
+        frame_rate = 15
+
+    else:
+        path_to_dir = Path("path_to_directory_with_sequence_of_frames")
+        target_dir = _results_dir_pathlib / "user_defined_directory_to_store_video"
+        video_name = "user_defined_video_name_without_extension"
+        frame_rate = 15
 
     video_editor = VideoEditor(target_directory=target_dir)
 
@@ -16,4 +34,5 @@ if __name__ == "__main__":
         target_video_name=video_name,
         fps=frame_rate,
         freeze_final_frame=True,
+        seconds_freezing_frame=1,
     )
