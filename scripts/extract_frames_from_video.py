28cdb6f4182b2970e2bd9866117a94e77e18c5c3
joan <joan.domenech.masferrer@gmail.com>
2021-11-11 16:50:56 +0100

    argparse path to video and target directory

--- scripts/extract_frames_from_video.py
+++ scripts/extract_frames_from_video.py
@@ -1,12 +1,31 @@
+import argparse
 from pathlib import Path
 
 from config import _results_dir_pathlib
 from video_editor_class import VideoEditor
 
 if __name__ == "__main__":
-    path_to_video = Path("path_to_video")
 
-    target_dir = _results_dir_pathlib / "user_defined_directory_to_store_frames"
+    parser = argparse.ArgumentParser("Extract frames from a video")
+    parser.add_argument("vid", nargs="?", help="path to video")
+    parser.add_argument("tdir", nargs="?", help="target dir to store video")
+    args = parser.parse_args()
+
+    path_to_video = args.vid
+    target_dir = args.tdir
+
+    if path_to_video is not None:
+        path_to_video = Path(path_to_video)
+        if target_dir is not None:
+            target_dir = Path(target_dir)
+        else:
+            target_dir = (
+                path_to_video.parents[0] / f"{path_to_video.stem}_extracted_frames"
+            )
+
+    else:
+        path_to_video = Path("path_to_video")
+        target_dir = _results_dir_pathlib / "user_defined_directory_to_store_frames"
 
     video_editor = VideoEditor(target_directory=target_dir)
 
