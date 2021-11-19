6e4c6f22e23b1e39d6bc07f23920e24c785c1201
joan <joan.domenech.masferrer@gmail.com>
2021-11-19 16:20:53 +0100

    Implement time measurements tracker

--- scripts/insert_timestamps_on_frame_sequence_in_dir.py
+++ scripts/insert_timestamps_on_frame_sequence_in_dir.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 import tqdm
@@ -14,19 +15,34 @@ def insert_timestamps_on_frame_sequence_in_dir(
     editor = ImageEditor(target_directory)
     list_of_files = sorted(path_to_directory.glob("*.png"))
 
+    time_measurements_tracker = dict.fromkeys(
+        set(dict_with_frame_id_and_message.values()), 0.0
+    )
+
     j = 0
     for i, file_path in tqdm.tqdm(enumerate(list_of_files), total=len(list_of_files)):
         try:
             frame_main_title = dict_with_frame_id_and_message[i]
             j = 0
         except KeyError:
-            pass
+            time_measurements_tracker[frame_main_title] += 1.0 / fps
 
         editor.set_current_img(path_to_image=file_path)
 
+        time_tracker_str = ""
+        for value in set(dict_with_frame_id_and_message.values()):
+            try:
+                time_tracker_str += f"\nTotal {value}:{round(time_measurements_tracker[value],3):.3f} seconds"
+            except KeyError:
+                time_tracker_str += f"\nTotal {value}:{round(0.0, 3):.3f} seconds"
+
         editor.insert_text_to_current_img(
-            text=f"{frame_main_title}...\nFrame #{j + 1}\n{round(j * (1 / fps), 3):.3f} seconds\nTotal:{round(j * (1 / fps), 3):.3f} seconds",
-            font_size=10,
+            text=f"{frame_main_title}...\n"
+            f"Frame #{j + 1} {round(j * (1 / fps), 3):.3f} seconds"
+            f"{time_tracker_str}"
+            # f"\nTotal {frame_main_title}:{round(time_measurements_tracker[frame_main_title], 3):.3f} seconds,"
+            f"\nTotal # frames: {i + 1} --- {round(i * (1 / fps), 3):.3f} seconds",
+            font_size=20,
             use_black_background=True,
         )
 
@@ -42,9 +58,24 @@ if __name__ == "__main__":
         # 23: "Title used from frame id 23 until next dict item"
     }
 
-    dir_path = Path("path_to_dir_with_frames")
+    parser = argparse.ArgumentParser("Extract frames from a video")
+    parser.add_argument("dir", nargs="?", help="path to directory with frames")
+    parser.add_argument("tdir", nargs="?", help="target dir to store video")
+    args = parser.parse_args()
 
-    target_dir = dir_path.parents[0] / f"{dir_path.stem}_extracted_frames"
+    dir_path = args.dir
+    target_dir = args.tdir
+
+    if dir_path is not None:
+        dir_path = Path(dir_path)
+        if target_dir is not None:
+            target_dir = Path(target_dir)
+        else:
+            target_dir = dir_path.parents[0] / f"{dir_path.stem}_edited_frames"
+
+    else:
+        dir_path = Path("path_to_dir_with_frames")
+        target_dir = dir_path.parents[0] / f"{dir_path.stem}_edited_frames"
     # target_dir = Path("user_defined_path")
 
     insert_timestamps_on_frame_sequence_in_dir(
