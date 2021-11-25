cfe50091ccead2e9b9a63952aff2a2255f939bb9
joan <joan.domenech.masferrer@gmail.com>
2021-11-25 20:22:05 +0100

    Support compositions with different lenghts

--- scripts/batch_create_side_by_side_frame_composition_from_dir.py
+++ scripts/batch_create_side_by_side_frame_composition_from_dir.py
@@ -16,6 +16,20 @@ def batch_create_side_by_side_frame_composition_from_dir(
     list_of_left_comp_files = sorted(path_to_frames_in_comp_left_side.glob("*.png"))
     list_of_right_comp_files = sorted(path_to_frames_in_comp_right_side.glob("*.png"))
 
+    left_comp_len = len(list_of_left_comp_files)
+    right_comp_len = len(list_of_right_comp_files)
+    if left_comp_len == right_comp_len:
+        pass
+    elif left_comp_len > right_comp_len:
+        last_right_comp_file = list_of_right_comp_files[-1]
+        for _ in range(left_comp_len - right_comp_len):
+            list_of_right_comp_files.append(last_right_comp_file)
+
+    elif right_comp_len > left_comp_len:
+        last_left_comp_file = list_of_left_comp_files[-1]
+        for _ in range(right_comp_len - left_comp_len):
+            list_of_right_comp_files.append(last_left_comp_file)
+
     number_of_files = len(list_of_left_comp_files)
 
     for i, left_image_path, right_image_path in tqdm.tqdm(
