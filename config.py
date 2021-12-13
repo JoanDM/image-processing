63533bfd321a475531f724ca2cc5540b7262202a
joan <joan.domenech.masferrer@gmail.com>
2021-12-13 23:55:26 +0100

    Add printer A4 DPI

--- config.py
+++ config.py
@@ -7,6 +7,11 @@ _results_dir_pathlib = Path(_base_dir) / "results"
 _resources_dir_pathlib = Path(_base_dir) / "resources"
 _tmp_dir_pathlib = _results_dir_pathlib / "tmp"
 
+PRINTER_DPI = 1200
+# For an A4 page, the size in pixels at 300dpi (without bleed area is (3508, 2480)
+A4_PIXEL_WIDTH_300DPI = 3508
+A4_PIXEL_HEIGH_300DPI = 2480
+
 
 def prRed(skk):
     print("\033[91m{}\033[00m".format(skk))
