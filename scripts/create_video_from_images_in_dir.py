f9f64bffd876f53d095d14abaf78ef33c6c67360
joan <joan.domenech.masferrer@gmail.com>
2022-02-16 21:38:42 +0100

    Fix bool args in argparse

--- scripts/create_video_from_images_in_dir.py
+++ scripts/create_video_from_images_in_dir.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
         "Create a video from a sequence of frames in a directory"
     )
     parser.add_argument(
-        "-dir", nargs="?", help="directory containing sequence of frames", type=str
+        "-dir", nargs="?", help="directory containing sequence of frames", type=Path
     )
     parser.add_argument(
-        "-tdir", nargs="?", help="target dir to store video", type=str, default=None
+        "-tdir", nargs="?", help="target dir to store video", type=Path, default=None
     )
     parser.add_argument("-vname", nargs="?", help="name for the video", type=str)
     parser.add_argument(
@@ -24,26 +24,24 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "-freeze",
-        nargs="?",
         help="freeze last frames for a second",
         action="store_true",
     )
 
     args = parser.parse_args()
-    directory = Path(args.dir)
     target_dir = args.tdir
     frame_rate = args.fps
     video_name = args.vname
     freeze_bool = args.freeze
 
     if target_dir is None:
-        target_dir = directory.parents[0] / f"{directory.stem}_video"
+        target_dir = args.dir.parents[0] / f"{args.dir.stem}_video"
 
     if video_name is None:
-        video_name = directory.stem
+        video_name = args.dir.stem
 
     video_editor.create_video_from_frames_in_dir(
-        path_to_directory=directory,
+        path_to_directory=args.dir,
         target_directory=target_dir,
         target_video_name=video_name,
         fps=frame_rate,
